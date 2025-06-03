420cce8d9b0e30ad2b457a7382e6073a3a1de5d3
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-03 17:46:44 -0600

    sidebar complete

--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,13 +1,24 @@
+import { useState } from "react";
+
 export default function Sidebar(){
+
+         const [menuOpen, setMenuOpen] = useState(false);
+
+         const toggleMenu = () => {
+         setMenuOpen(!menuOpen);
+        };
+    
     return(
         <div className="bar">
             <h1 className="logo">DR</h1>
-            <button className="button" >
-                <div className="lines">
+            <button className={`btn-menu ${menuOpen ? 'open' : ''}`}
+                onClick={toggleMenu}
+            >
+                
                     <div className="line"></div>
                     <div className="line"></div>
                     <div className="line"></div>
-                </div>
+                    
             </button>
         </div>
     )
