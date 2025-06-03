885b35e0bea1566b41737bdae6a22d9bbfa46115
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-03 16:13:08 -0600

    setting changes up

---
+++ src/Components/Sidebar.jsx
@@ -0,0 +1,14 @@
+export default function Sidebar(){
+    return(
+        <div className="bar">
+            <h1 className="logo">DR</h1>
+            <button className="button" >
+                <div className="lines">
+                    <div className="line"></div>
+                    <div className="line"></div>
+                    <div className="line"></div>
+                </div>
+            </button>
+        </div>
+    )
+}
