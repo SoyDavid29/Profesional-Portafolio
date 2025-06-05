3c95e5b05266fb0800a4fb8d4de552d4305dd4a5
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-04 20:40:53 -0600

    first animation complete in profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Profile from "./Components/Profile"
 import Sidebar from "./Components/Sidebar"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
      <Sidebar/>
+     <Profile/>
     </>
   )
 }
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,14 @@
+export default function Profile(){
+    return(
+        <section className="profile-animation">
+            <div className="animation">
+                <div className="figures">
+                    <div className="figure" >
+                        <img className="img" src="/img/yo.jpg" alt="picture of david" />
+                    </div>
+                </div>
+        
+            </div>
+        </section>
+    )
+}
