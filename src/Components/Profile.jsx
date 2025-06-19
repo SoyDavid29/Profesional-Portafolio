bb4a2a471eb0aafa474e19ce1322019688e150ee
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-19 17:38:20 -0600

    modifications on  my images

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,23 +1,23 @@
-export default function Profile(){
-    return(
+export default function Profile() {
+    return (
         <section className="profile-animation">
-            
 
-                <div className="animation" >
 
-                     <div className="figure" >
-                        
-                    </div><img className="img" src="/Profesional-Portafolio/img/yo.jpg" alt="picture of david" />
+            <div className="animation" >
 
-                    <div className="texts">
-                    <h1 className="uxui"><span className="online"></span> Frontend Developer and UX/UI Designer</h1>
-                    <p className="on"><span className="circle"></span>Available</p>
-                    </div>
-
+                <div className="figure" >
+                      <img className="img" src="/Profesional-Portafolio/img/yo.jpg" alt="picture of david" />
                 </div>
 
-                
-                
+                <div className="texts">
+                    <h1 className="uxui"><span className="online"></span> Frontend Developer and UX/UI Designer</h1>
+                    <p className="on"><span className="circle"></span>Available</p>
+                </div>
+
+            </div>
+
+
+
         </section>
     )
 }
