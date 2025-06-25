bb880dc29cf65d92b80ab93ffcb4d005706334ea
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-25 13:54:22 -0600

    changes in Projects.jsx component

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,14 +1,28 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faArrowUpRightFromSquare } from '@fortawesome/free-solid-svg-icons'
 
-export default function Projects(){
-    return(
+export default function Projects() {
+    return (
 
-        <section className="projects-container">
-            <div className="project">
-                <img className="img-proyect" src="/Profesional-Portafolio/img/fundacion.jpg" alt="project fundation mgas" />
-                <a className="link-project" href="https://fundacionmgas.org.mx/">FUNDACIÓN MGAS <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+        <section className='projects'>
+
+            <div className="projects-container">
+                <div className="project">
+                    <img className="img-proyect" src="/Profesional-Portafolio/img/fundacion.jpg" alt="project fundation mgas" />
+                    <a className="link-project" href="https://fundacionmgas.org.mx/">FUNDACIÓN MGAS <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+                </div>
             </div>
+
+            <div className="projects-container">
+                <div className="project">
+                    <img className="img-proyect" src="/Profesional-Portafolio/img/fundacion.jpg" alt="project fundation mgas" />
+                    <a className="link-project" href="https://fundacionmgas.org.mx/">FUNDACIÓN MGAS <FontAwesomeIcon icon={faArrowUpRightFromSquare} /></a>
+                </div>
+            </div>
+
+
         </section>
+
+
     )
 }
