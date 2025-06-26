888715621a755e970bb2eb209ff325b6d633eaf1
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-26 13:55:54 -0600

    add form

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,5 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMessage, faLocationDot, faRocket } from '@fortawesome/free-solid-svg-icons'
+import ContactForm from './ContactForm'
 
 export default function Contact() {
     return (
@@ -17,28 +18,34 @@ export default function Contact() {
                 <div className="card">
                     <FontAwesomeIcon icon={faMessage} className='icon' />
                     <h3>Sent me an E-mail</h3>
-                    <p>You can send me an email, and I will respond immediately.</p>
+                    <p className="text-card" >You can send me an email, and I will respond immediately.</p>
                     <a className='mail' href="mailto:daviswebdesigner@gmail.com?subject=Hello&body=Hi there!">
-                       daviswebdesigner@gmail.com
+                        daviswebdesigner@gmail.com
                     </a>
-                </div> 
+                </div>
 
                 <div className="card">
-                     <FontAwesomeIcon icon={faLocationDot} />
-                     <h3>Location</h3>
-                     <p>"I work remotely from Durango, Mex, offering services throughout Mexico."</p>
-                     <p>"Durango, Mexico."</p>
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <h3>Location</h3>
+                    <p className="text-card" >I work remotely from Durango, Mex, offering services throughout Mexico.</p>
+                    <p className="text-card1" >Durango, Mexico.</p>
                 </div>
             </div>
 
             <div className='form-container'>
                 <div className="card">
-                     <FontAwesomeIcon icon={faRocket} />
-                     <h3>Complete the form and I'll respond personally within 24 hours.</h3>
-                     <p>"Let's talk about how can i help you!."</p>
+                    <FontAwesomeIcon icon={faRocket} />
+                    <h3>Complete the form and I'll respond personally within 24 hours.</h3>
+                    <p className="text-card">Let's talk about how can i help you!.</p>
                 </div>
+                
+
+                <ContactForm/>
+        
+        
+                
             </div>
-            
         </section>
     )
+
 }
---
+++ src/Components/ContactForm.jsx
@@ -0,0 +1,52 @@
+import { useForm, ValidationError } from '@formspree/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faMessage, faLocationDot, faRocket } from '@fortawesome/free-solid-svg-icons'
+
+export default function ContactForm() {
+
+  const [state, handleSubmit] = useForm("mldgqqoy");
+  if (state.succeeded) {
+      return <p>Thanks for joining!</p>;
+  }
+
+  return (
+    <form className='form' onSubmit={handleSubmit}>
+
+      <label htmlFor="name">Your name:</label>
+      <input type="text" id="name" name="name"/>
+
+      <label htmlFor="email">
+        Email Address:
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+       
+       
+      <label htmlFor="text">Messege:</label>
+      <textarea
+        id="message"
+        name="message"
+      />
+
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+
+      <button className='send-button' type="submit" disabled={state.submitting}>
+        Send <FontAwesomeIcon icon={faRocket} />
+      </button>
+
+    </form>
+  );
+
+}
