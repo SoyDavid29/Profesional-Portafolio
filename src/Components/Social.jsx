9cbcfce91a5a10bcafcfd20b196d18eeb7afb913
DavidRodriguez <studiandoprogramacion@gmail.com>
2025-06-30 16:14:14 -0600

    slight changes

--- src/Components/Social.jsx
+++ src/Components/Social.jsx
@@ -7,15 +7,14 @@ export default function Social() {
         <div className="social-container">
 
             <div className="social">
-                <h1 className='design-tittle'>DR DESIGN<span className='david-tittle'>David Rodriguez</span></h1>
+                <h1 className='design-tittle'>DR DESIGN </h1>
                 <p className='david-tittle1' >Custom web development, with a balance of design and functionality.</p>
             </div>
 
             <div className="logos-container">
-                <p className='david-tittle1'>David Rodriguez</p>
-                <FontAwesomeIcon className='likedin-logo' icon={faLinkedin} />
+                <p className='david-tittle2'>David Rodriguez</p>
+                <a className='likedin-logo' href="https://www.linkedin.com/in/soydavidr/"><FontAwesomeIcon className='likedin' icon={faLinkedin} /></a>
             </div>
-
         </div>
     )
 }
